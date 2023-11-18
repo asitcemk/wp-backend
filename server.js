6f8fcde974fc5dc07f75a623902cf852892a89ad
Asit Senapati <asitsenapati@Asits-MacBook-Air.local>
2023-11-18 14:04:53 +0530

    allow cros

--- server.js
+++ server.js
@@ -25,11 +25,7 @@ app.use(bodyParser.json())
 app.set('view engine', 'jade');
 
 // routes
-app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
+app.options('*', cors());
 
 require('./routes/index')(app);
 
